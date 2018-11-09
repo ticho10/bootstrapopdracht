d092628062b8ce2a9e7b0a8d4c70eb49a4160886
Poffertjeskip <30628312+ticho10@users.noreply.github.com>
2018-11-09 12:38:18 +0100

    Add files via upload
    http://25030.hosts1.ma-cloud.nl/bewijzenmap/periode2.1/fro/opdracht3/

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap opdraacht FRO</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <img src="ma.jpg" alt="MA" style="width: 200px;">
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">MA</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Opleiding</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Heb je nog vragen</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Digitale licentie actieveren</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+
+
+<div class="container" style="margin-top:30px">
+        <div class="col-sm-12">
+            <h1>Opdagen</h1>
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="dag.jpg" style="height: 300px;">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="henk.jpg" style="height: 300px;">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="buiten.jpg" alt="Third slide" style="height: 300px;">
+                    </div>
+                </div>
+            </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            <div class="col-sm-6" style="float: right">
+            <br>
+            <p style="font-size: 20px;">Ma helpt als jij het wilt maken in de media. Met opleidingen die je voorbereiden op jouw toekomstige beroep. Om hier alvast kennis mee te maken bezoek je natuurlijk één van onze open dagen. Dat is dé manier om de school en de beroepen waar we voor opleiden te leren kennen. Kijk of jij er tussen past. Kom langs, proef de sfeer, ontmoet medewerkers en studenten, en bekijk hun werk in onze lokalen en studio’s.</p>
+        </div>
+
+        <div class="col-sm-6" style="">
+            <h2>MediaDeveloper</h2>
+            <h5>In iedereen schuilt talent. Het gaat er om jouw specifieke talent te ontdekken en te ontwikkelen. Ma helpt. We hebben 14 mbo-opleidingen en een vmbo-opleiding waarmee je kunt worden wat je bent.</h5>
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="hans.jpg" style="max-height: 300px;">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="media.jpg" style="max-height: 3000px;">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="kip.jpg" alt="Third slide" style="max-height: 300px;">
+                    </div>
+                </div>
+            </div>
+            <hr class="d-sm-none">
+        </div>
+    </div>
+
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <p>MediaCollege Amsterdam</p>
+</div>
+
+</body>
+</html>
+
